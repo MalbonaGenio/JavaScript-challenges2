4eca66996e4b7f740e9f61b0a30ed10188ff1998
MalbonaGenio <pvalverdemolina@gmail.com>
2020-11-21 01:46:12 +0000

    added the import export challenge. in data.js we define a function an export it to the index.js where we will pass the arguments and store the return in a variale to console.log it. In the html we needed to set up a type module so it does not reurn an error.

---
+++ import-export/index.js
@@ -0,0 +1,4 @@
+import { add } from './data.js'
+
+let result = add(1,4) // passes the two numbers as arguments and stores the return of add function in the ./data.js into result
+console.log(result)
