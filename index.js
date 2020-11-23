f04d24ffe1630eddbcf11404697c25861667323f
MalbonaGenio <pvalverdemolina@gmail.com>
2020-11-23 00:03:37 +0000

    first part of the challenge completed. set up a class and a constructor to assigne the values to the class. Then created a funtcion that takes the arguments used when we create a new instanec (in this case a new player) and prints the info.

--- index.js
+++ index.js
@@ -10,3 +10,21 @@
 // - Add a function that when it runs should print into the console something similar ("Rafael Nadal is 34 years old and knows how to play Tennis");
 // - Make sure the Name and Age are dynamic.
 
+class Player {
+	//asign the values to class Player
+	constructor (name, country) {
+		this.name = name
+		this.country = country
+	}
+
+	//creates a function to print the name and the country in a dynamic way depending on the instances created.
+	//need to use this.name to reference to the values inside the class without it it will not work.
+	playerInfo () {
+		console.log(`${this.name}, was born in ${this.country}.`)
+	}
+}
+
+//creates an instance of a player using the Player class
+let iu = new Player("Iu", "Catalonia")
+//calls the function inside the class, it will use the argument set in the instance creation
+iu.playerInfo()
