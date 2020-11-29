2591b579e85e2593105a18a3e7efcf9d28fd45d1
MalbonaGenio <pvalverdemolina@gmail.com>
2020-11-29 08:46:38 +0000

    added the small challenge for the switch conditional another way to do conditional flow in JS that does not use the if if else syntax and is more suited for strings or easier to read to me.

---
+++ switch-conditional/index.js
@@ -0,0 +1,12 @@
+const time = "morning" // could be "afternoon", "evening" and "night"
+
+switch (time) {
+    case "morning":
+        console.log("Good morning!")
+        break
+    case "evening":
+        console.log("Good Evening!")
+        break
+    default:
+        console.log("Good day to you!")
+}
